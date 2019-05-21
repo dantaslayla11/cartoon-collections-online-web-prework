e4e283ce42094fa80346f7b8933ada0341ea5c1f
layla dantas <laylatdantas@gmail.com>
2019-05-21 23:55:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,38 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+ dwarves = array.map.with_index(1) {|ch, idx| [idx, ch]}
+ puts dwarves.join(" ")
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet(array)
+  array.map {|string1| string1.capitalize << "!"} 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+
+
+
+def long_planeteer_calls(array)
+  i = 0 
+  array.each do |item|
+    puts item
+    if (item.length > 4)
+      return true
+    elsif item.length<=4
+      i=i+1;
+    end
+  end
+  puts i 
+  if i == array.length 
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
+
+
+def find_the_cheese
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
